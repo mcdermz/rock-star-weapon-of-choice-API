6e517626cef32840a293b40c49cccf2c4474fb06
Sean McDermott <mcderm.sean@gmail.com>
2017-06-24 14:35:52 -0700

    added hint links for the front end

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,15 @@
-console.log('sanity');
 $('#request-btn').click(() => {
   const endpoint = $('#request-url-input').val()
   const url = 'http://localhost:3000/api/' + endpoint
+
   $.ajax(url).then(data => {
     $('#responseOutput').empty().append(JSON.stringify(data, null, 2))
   })
+})
+
+$('.hint').on('click', 'i', function (e){
+  const endpoint = e.target.innerText
+
+  $('#request-url-input').val(endpoint)
+  $('#request-btn').click()
 })
